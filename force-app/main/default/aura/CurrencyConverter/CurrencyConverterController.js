1756f47496ff59125ce5611120d8ce080ae52722
deepak <deepaksoniitm@gmail.com>
2023-05-23 12:21:39 +0530

    changes remove

--- force-app/main/default/aura/CurrencyConverter/CurrencyConverterController.js
+++ force-app/main/default/aura/CurrencyConverter/CurrencyConverterController.js
@@ -12,7 +12,7 @@
                 }
                 console.log('value here',value);
                 component.set("v.code1",value);
-                component.set("v.code2",value add);
+                component.set("v.code2",value);
 
                 
             }else{
